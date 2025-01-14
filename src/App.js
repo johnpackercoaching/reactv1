8a878f3bbede2e097570c8ada18aaaf11a08b935
John Packer <john@johnpackercoaching.com>
2025-01-13 23:17:33 -0700

    updated word

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactionz
+          Learn Reactionss
         </a>
       </header>
     </div>
