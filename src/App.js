420a49144c3d3f613270fc17d1ac752471ee0c45
John Packer <john@johnpackercoaching.com>
2025-01-15 09:51:27 -0700

    Fix typo in link text from "Learn Reactionss" to "Learn Reactionsss"

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactionss
+          Learn Reactionsss
         </a>
       </header>
     </div>
