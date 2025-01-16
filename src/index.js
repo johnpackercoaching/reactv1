a20ec27468d8c908a083783bb41c1696321e52a7
John Packer <john@johnpackercoaching.com>
2025-01-15 17:19:14 -0700

    Remove unused files and simplify the application structure by deleting CSS, test, and utility files.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <App />
   </React.StrictMode>
 );
